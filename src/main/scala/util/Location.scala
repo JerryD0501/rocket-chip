a80ecb9b7dc9b6f42dd48ffa683785a7d201ec4a
Henry Cook <henry@sifive.com>
2020-03-25 17:01:06 -0700

    tighten up LocationMap API

--- src/main/scala/util/Location.scala
+++ src/main/scala/util/Location.scala
@@ -5,24 +5,27 @@ package freechips.rocketchip.util
 import scala.language.dynamics
 import scala.collection.mutable.Map
 
-class Location[T](val name: String)
+class Location[T](val name: String) extends Dynamic {
+  def applyDynamic[A](portname: String)(args: A*): (Location[A], A) = {
+    require(args.size == 1, "Location: can't support multiple things at one port yet")
+    (new Location[A](s"${name}_${portname}"), args.head)
+  }
+}
 
-class LocationMap[T] private(lm: Map[String, T]) extends Map[Location[T], T] with Dynamic {
-  private val internalMap = lm
-  def +=(kv: (Location[T], T)) = new LocationMap[T](internalMap + (kv._1.name -> kv._2)).asInstanceOf[this.type]
-  def -=(key: Location[T]) = new LocationMap(internalMap - key.name).asInstanceOf[this.type]
-  def get(key: Location[T]) = internalMap.get(key.name)
+object Location {
+  def apply[T](name: String): Location[T] = new Location[T](name)
+}
+
+class LocationMap[T] private (val internalMap: Map[String, T]) extends Map[Location[_], T] {
+  def +=(kv: (Location[_], T)) = { (internalMap += (kv._1.name -> kv._2)); this }
+  def -=(key: Location[_]) = { (internalMap -= key.name); this }
+  def get(key: Location[_]) = internalMap.get(key.name)
   def iterator = internalMap.iterator.map(kv => (new Location(kv._1), kv._2))
-
-  def select(location: Location[T]): T = selectDynamic(location.name)
-  def selectDynamic(name: String): T = {
-    internalMap.getOrElse(name, throw new Exception(s"could not find location named $name"))
-  }
-
-  def update(location: Location[T])(value: T): Unit = updateDynamic(location.name)(value)
-  def updateDynamic(name: String)(value: T): Unit = {
-    internalMap += name -> value
-  }
+  // TODO override def default to provide specific exception on missing location?
+  // TODO figure out how to be more clever about applying sub-type casting
+  //      for other the other Map trait methods
+  def required[L <: T](key: Location[L]): L = internalMap(key.name).asInstanceOf[L]
+  def optional[L <: T](key: Location[L]): Option[L] = internalMap.lift(key.name).map(_.asInstanceOf[L])
 }
 
 object LocationMap {
